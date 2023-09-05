c6e2e7bd6cef95780699bd390bfe9d533c9a2e39
Yash <yash@pop-os.localdomain>
2023-09-05 14:51:10 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,41 @@
+import logo from "./logo1.svg";
+import skill1 from "./skill1.png";
+import skill2 from "./skill2.png";
+import web2 from "./web2.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import css from "./tech/css.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png"
+import bootstrap from "./tech/bootstrap.png"
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.png";
+import tindog from "./tindog.png";
+import simon from "./simon.png";
+import tagit from "./tagit.png";
+
+export {
+  logo,
+  skill1,
+  skill2,
+  web2,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  html,
+  javascript,
+  nodejs,
+  bootstrap,
+  reactjs,
+  tailwind,
+  threejs,
+  tindog,
+  simon,
+  tagit,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,154 @@
+import {
+  web2,
+  skill1,
+  skill2,
+  web,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  bootstrap,
+  simon,
+  tindog,
+  tagit,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Web Developement",
+    icon: web,
+  },
+  {
+    title: "Responsive Web Designs",
+    icon: web2,
+  },
+  {
+    title: "HTML, CSS, JS, React JS",
+    icon: skill1,
+  },
+  {
+    title: "DSA, C++, SQL, OS, Cloud",
+    icon: skill2,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  {
+      name: "Bootstrap",
+      icon: bootstrap,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+];
+
+const projects = [
+  {
+    name: "TagIT",
+    description:
+      "TagIT is a user-friendly and innovative online platform designed which offers a unique QR code generator that enables travelers to create personalized QR codes and attach them to their bags, with a Live Chat feature.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tagit,
+    source_code_link: "https://github.com/TijilM/TagIt",
+  },
+  {
+    name: "Simon Game",
+    description:
+      "The 'Simon Memory Game' is a web-based implementation of the classic electronic game 'Simon,' designed to challenge and improve a player's memory and pattern recognition skills.",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: simon,
+    source_code_link: "https://y-ashrathore.github.io/SimonGame/",
+  },
+  {
+    name: "Tin Dog",
+    description:
+      "TinDog is a fun and interactive website that brings together dog owners and their furry companions in a playful and social manner. Modeled after the popular dating app Tinder.",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tindog,
+    source_code_link: "https://y-ashrathore.github.io/TinDog/",
+  },
+];
+
+export { services, technologies, projects };
