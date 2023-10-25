d8aa34fcc38981d1def3bac4cccb1ab087a24dad
Y-ash Rathore <meyashvardhan36@gmail.com>
2023-10-25 22:29:13 +0530

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+    const year = new Date().getFullYear();
+    return (
+        <footer>
+            <a href="https://github.com/Y-ashRathore"><i class="fab fa-github footer-logo"></i></a>
+
+            <a href="https://www.instagram.com/ya.sh.r/"><i class="fab fa-instagram footer-logo"></i></a>
+            <p>Copyright ⓒ {year} Yash Rathore</p>
+        </footer>
+    );
+}
+
+export default Footer;
